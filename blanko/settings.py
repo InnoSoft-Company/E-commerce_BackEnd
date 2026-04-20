53c64dc590ac66d3aa7d0f2c85a9dcf6d0598ba3
Mohammed Ahmed Ghanam <midoghanam@hotmail.com>
2026-04-20 14:24:35 +0200

    Changing "settings.py"

--- blanko/settings.py
+++ blanko/settings.py
@@ -17,7 +17,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     # Third party
-    "rest_framework", "corsheaders",
+    "rest_framework", 
     "rest_framework_simplejwt",
     "rest_framework_simplejwt.token_blacklist",
     "corsheaders", 'drf_spectacular', 
